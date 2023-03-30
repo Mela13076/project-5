18a8c89b2379d45c25410d64206f76c2e18359e9
Amelia Trevino <email@example.com>
2023-03-30 01:45:15 -0500

    updates

--- rick-and-morty-characters/src/componets/content.jsx
+++ rick-and-morty-characters/src/componets/content.jsx
@@ -1,6 +1,7 @@
 import Filter from "./filter"
 import CharacterInfo from "./characterInfo"
 import { useEffect, useState } from "react";
+import Dashboard from "./dashboard";
 
 function Content(){
     const [characters, setCharacters] = useState([]);
@@ -9,17 +10,6 @@ function Content(){
     useEffect(() => {
         async function fetchData() {
         try {
-            // const response = await fetch('https://rickandmortyapi.com/api/character');
-            // const data = await response.json();
-            // setCharacters(data.results);
-            // setFilteredCharacters(data.results);
-            // const response1 = await fetch("https://rickandmortyapi.com/api/character");
-            // const data1 = await response1.json();
-            // const response2 = await fetch("https://rickandmortyapi.com/api/character?page=2");
-            // const data2 = await response2.json();
-            // const response3 = await fetch("https://rickandmortyapi.com/api/character?page=3");
-            // const data3 = await response3.json();
-            // const allCharacters = data1.results.concat(data2.results, data3.results);
             async function fetchAllCharacters() {
                 let allCharacters = [];
                 let page = 1;
@@ -53,6 +43,9 @@ function Content(){
   };
     return(
         <div className="Body">
+          <div className="dash-sec">
+            <Dashboard filteredCharacters={filteredCharacters}/>
+          </div>
             <div className="top-content">
                 {/* <Filter setFilteredCharacters={setFilteredCharacters}/> */}
                 <Filter characters={characters} setFilteredCharacters={setFilteredCharacters} handleFilter={handleFilter}/>
